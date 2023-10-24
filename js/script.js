218ae23ec3ad9bf43517450027707b47f6c2dddf
naachocaceress <naachocaceress@gmail.com>
2023-10-24 20:03:36 -0300

    Version 2.0

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+// window.addEventListener('scroll', function() {
+//     let header = document.getElementById('header');
+//     let boca = this.document.getElementById("wave")
+//     let scrollPosition = window.scrollY
+//     let scrollPositionboca = boca.offsetTop
+//     if (scrollPosition >= scrollPositionboca) { // Cambia el valor según cuánto quieras que se desplace antes de cambiar el color
+//       header.style.backgroundColor = "rgba(128, 128, 128, 0.096)";
+//     } else {
+//       header.style.backgroundColor = "whitesmoke"; // Cambia de nuevo al color original
+//     }
+//   });
