fbfa3b23fb3fab10173a3a4e3e04257e12d077e2
naachocaceress <naachocaceress@gmail.com>
2023-12-05 02:07:48 -0300

    Version 2.7 - Listo index

--- js/script.js
+++ js/script.js
@@ -13,13 +13,6 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
             return new bootstrap.Tooltip(tooltipTriggerEl)
         })
 
-
-        window.onload = function() {
-          document.querySelector("#left .inicio").style.transform = "translateX(0)";
-          document.querySelector("#right .inicio").style.transform = "translateX(0)";
-        };
-        
-
 const scrollers = document.querySelectorAll(".scroller");
 
 // If a user hasn't opted in for recuded motion, then we add the animation
@@ -47,7 +40,17 @@ function addAnimation() {
   });
 }
 
-  
+
+var svg = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8"/> </svg>';
+
+var links = document.querySelectorAll('.linkAPagina');
+links.forEach(function(link) {
+    link.innerHTML += svg;
+});
+
+
+AOS.init();
+
 
 
 
